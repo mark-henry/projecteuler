83b5813c3489e3382a24e24599dcadcca9180296
Mark Henry <markhenry.software@gmail.com>
2015-09-11 10:35:11 -0700

    Converted pandigital library to generator comprehensions

--- pandigitals.py
+++ pandigitals.py
@@ -1,4 +1,6 @@
 import itertools
 
-onetonine = map(''.join, set(itertools.permutations('123456789')))
-zerotonine = map(''.join, set(itertools.permutations('0123456789')))
+# The set of all possible arrangements of the numbers 1-9
+onetonine = (''.join(seq) for seq in itertools.permutations('123456789'))
+# The set of all possible arrangements of the numbers 0-9
+zerotonine = (''.join(seq) for seq in itertools.permutations('0123456789'))
