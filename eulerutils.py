9a84c2c0ab0bd52f6d784ac2fdab9932f17cabd3
Mark Henry <markhenry.software@gmail.com>
2015-09-18 12:56:42 -0700

    Add solution for 54

--- eulerutils.py
+++ eulerutils.py
@@ -1,3 +1,6 @@
+import itertools
+
+
 def explode_digits(n):
 	digits = []
 	while n:
@@ -5,9 +8,26 @@ def explode_digits(n):
 		n /= 10
 	return digits
 
+
 def implode_digits(digits):
 	n = 0
 	for d in digits:
 		n *= 10
 		n += d
 	return n
+
+
+def is_palindrome(num):
+	digits = explode_digits(num)
+	for i in range(len(digits)/2):
+		if digits[i] != digits[(-i) - 1]:
+			return False
+	return True
+
+
+# ###
+# Pandigitals
+# The set of all possible arrangements of the numbers 1-9
+onetonine = (''.join(seq) for seq in itertools.permutations('123456789'))
+# The set of all possible arrangements of the numbers 0-9
+zerotonine = (''.join(seq) for seq in itertools.permutations('0123456789'))
